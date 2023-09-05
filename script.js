580faf9cb53a84dd9f53d083f03218202fcabd51
Marine Valentin <Lamarine@MacBook-Air.local>
2023-09-05 17:32:33 +0200

    ???

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 if (window.location.pathname.endsWith('recap.html')) {
     const urlParams = new URLSearchParams(window.location.search);
     
-    // Récupération des données depuis les paramètres d'URL
     const client = urlParams.get('client');
     const date = urlParams.get('date');
     const observation = urlParams.get('observation');
 
-    // Affectation des données récupérées aux éléments de recap.html
+
     document.getElementById('clientName').textContent = client;
     document.getElementById('interventionDate').textContent = date;
     document.getElementById('observationDetails').textContent = observation; 
