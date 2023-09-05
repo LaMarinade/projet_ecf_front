987656bca6e4b4be084e84b290dea07f15becb2c
Marine Valentin <Lamarine@MacBook-Air.local>
2023-09-05 17:16:15 +0200

    responsive et footer

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 if (window.location.pathname.endsWith('recap.html')) {
     const urlParams = new URLSearchParams(window.location.search);
-    const nom = urlParams.get('nom');
+    
+    // Récupération des données depuis les paramètres d'URL
+    const client = urlParams.get('client');
     const date = urlParams.get('date');
-    const vitrerie = urlParams.get('vitrerie');
-    const aspirateur = urlParams.get('aspirateur');
-    const entretien = urlParams.get('Entretien');
-
-    let interventionTypes = [];
-    if (vitrerie) interventionTypes.push('Vitrerie');
-    if (aspirateur) interventionTypes.push('Aspirateur');
-    if (entretien) interventionTypes.push('Entretien général');
-
-    const interventionString = interventionTypes.join(', '); //"join" pour afficher les éléments à la suite
+    const observation = urlParams.get('observation');
 
     // Affectation des données récupérées aux éléments de recap.html
-    document.getElementById('clientName').textContent = nom;
+    document.getElementById('clientName').textContent = client;
     document.getElementById('interventionDate').textContent = date;
-    document.getElementById('interventionType').textContent = interventionString;
+    document.getElementById('observationDetails').textContent = observation; 
 }
